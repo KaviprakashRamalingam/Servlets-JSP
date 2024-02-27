54e307d4be93dfe43457ddcd21fac27f61622190
KaviprakashRamalingam <ramalingam.ka@northeastern.edu>
2024-02-27 13:24:27 -0500

    Create Movie.java

---
+++ ContollerNameHandler/src/main/java/com/mycompany/hw4_4/pojo/Movie.java
@@ -0,0 +1,53 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.hw4_4.pojo;
+
+/**
+ *
+ * @author kaviprakashramalingam
+ */
+public class Movie {
+
+    private String Title;
+    private String Actor;
+    private String Actress;
+    private String Genre;
+
+    public Movie() {
+    }
+
+    public String getTitle() {
+        return Title;
+    }
+
+    public void setTitle(String Title) {
+        this.Title = Title;
+    }
+
+    public String getActor() {
+        return Actor;
+    }
+
+    public void setActor(String Actor) {
+        this.Actor = Actor;
+    }
+
+    public String getActress() {
+        return Actress;
+    }
+
+    public void setActress(String Actress) {
+        this.Actress = Actress;
+    }
+
+    public String getGenre() {
+        return Genre;
+    }
+
+    public void setGenre(String Genre) {
+        this.Genre = Genre;
+    }
+    
+}
