1c01bcdf9a3785d9793dbfdf0d0d9fee23a5d0d7
KaviprakashRamalingam <ramalingam.ka@northeastern.edu>
2024-02-26 22:25:32 -0500

    Create Book.java

---
+++ Bookstore_Spring_MVC/src/main/java/com/mycompany/lab6/pojo/Book.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.lab6.pojo;
+
+/**
+ *
+ * @author kaviprakashramalingam
+ */
+public class Book {
+    private String isbn;
+    private String title;
+    private String author;
+
+    public Book() {
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+    
+    
+}
