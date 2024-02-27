e5730dd0bca2307f92dc6c20b80dce675f971fdc
KaviprakashRamalingam <ramalingam.ka@northeastern.edu>
2024-02-26 22:25:34 -0500

    Create UrlController.java

---
+++ Bookstore_Spring_MVC/src/main/java/com/mycompany/lab6/UrlController.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.lab6.controller;
+
+import java.io.PrintWriter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.AbstractController;
+
+/**
+ *
+ * @author kaviprakashramalingam
+ */
+public class UrlController extends AbstractController{
+    @Override
+    protected ModelAndView handleRequestInternal(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.println("<!DOCTYPE html>");
+        out.println("<html>");
+        out.println("<head>");
+        out.println("<title>My URL Controller</title>");
+        out.println("</head>");
+        out.println("<body>");
+        out.println("<p>Hi this is url controller</p>");
+        out.println("</body>");
+        out.println("</html>");
+        return null;
+    }
+}
